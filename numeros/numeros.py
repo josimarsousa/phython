b36845817830eedbbad81f99d016c08633825219
josimarsousa <josimarjosesousa@gmail.com>
2021-11-24 21:53:28 -0300

    first commit

---
+++ numeros/numeros.py
@@ -0,0 +1,15 @@
+x = 5
+y = 10
+z = 3
+
+soma = x + y
+divisao = y / x
+multiplicacao = x * y
+divisaoDiscartaDecimal = y // z
+retornaRestoDivisao = x % 2
+calculandoPotencia = x ** 2
+
+print(soma, divisao, multiplicacao, divisaoDiscartaDecimal, retornaRestoDivisao, 
+calculandoPotencia)
+
+
