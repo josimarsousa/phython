4884b9e5805bba07ed688c94b389926861096c8c
josimarsousa <josimarjose@hotmail.com>
2021-11-27 16:16:45 -0300

    api conversao moeda pronto

--- apis/conversaoMoeda.py
+++ apis/conversaoMoeda.py
@@ -4,8 +4,15 @@ url = 'https://api.exchangerate-api.com/v6/latest'
 
 req = requests.get(url)
 resp =  req.status_code
+dados = req.json()
 
 if resp == 200:
-  print('ok')
+  print('Dados encontrados: \n', dados, '\n')
+
 else:
   print('Nao encontrado!')
+
+valor_reais = float(input('Informe o valor em R$ a ser convertido:\n'))
+cotacao = dados['rates']['BRL']
+print(f'R${valor_reais} em dolar valem US$ {(valor_reais / cotacao):.2f}')
+
